a9aead328a0a0ad42e90b0f54b2265ee2ba3d84f
John DeRiggi <j_deriggi@yahoo.com>
2012-03-26 23:04:54 -0400

    missing final curly!

--- src/LineOfSightUtils.java
+++ src/LineOfSightUtils.java
@@ -56,3 +56,4 @@ public class LineOfSightUtils {
     public static double distance(double x1, double y1, double z1, double x2, double y2, double z2){
         return Math.sqrt( Math.pow(x1 - x2, 2) +  Math.pow(y1 - y2, 2)  +  Math.pow(z1 - z2, 2)  );
     }
+}
